32a0f765b5e6d40092de35e4170d76abe2057fa7
Kaizad Avari <kaizad.avari@live.com>
2017-04-10 23:04:44 -0400

    Now all staged

--- app/src/main/java/edu/uf/mc/sauce/toomuchsos/CrashDetectionService.java
+++ app/src/main/java/edu/uf/mc/sauce/toomuchsos/CrashDetectionService.java
@@ -1,9 +1,150 @@
 package edu.uf.mc.sauce.toomuchsos;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.support.annotation.IntegerRes;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Kaizad on 4/9/2017.
  */
-public class CrashDetectionService extends Service {
+public class CrashDetectionService extends Service implements ShakeDetector.OnShakeListener
+{
+    private static final int REMINDER_NOTIFICATION = 11;
+    private NotificationManager mNM;
+
+    int currentSpeed;
+    boolean isPiezo, isGforce;
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private Vibrator vibrator;
+
+    //Create an inner Binder class
+    public class SOSBinder extends Binder
+    {
+        public CrashDetectionService getService()
+        {
+            return CrashDetectionService.this;  //Return this to access the serice later.
+        }
+    }
+
+    private final IBinder binder = new SOSBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        //Toast.makeText(this, "STICKY service started", Toast.LENGTH_SHORT).show();
+        if(intent != null && intent.getStringExtra("Speed") != null)
+        {
+            Log.d("SSSSSPPPEEEEEDDDDD", currentSpeed + "");
+            currentSpeed = Integer.parseInt(intent.getStringExtra("Speed"));
+            Toast.makeText(this, "Started with " + currentSpeed, Toast.LENGTH_SHORT).show();
+        }
+        super.onStartCommand(intent, flags, startId);
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        showNotification();
+        Toast.makeText(this, "Service initialized", Toast.LENGTH_SHORT).show();
+        super.onCreate();
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(this);
+
+        //Register the listener as soon as service starts
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onShake(int count)
+    {
+        Toast.makeText(this, "SHHAAAAAKKKKKKEEEEEEE!", Toast.LENGTH_LONG).show();
+        if(vibrator.hasVibrator())
+            vibrator.vibrate(500);
+    }
+
+
+    private void showNotification()
+    {
+        // The PendingIntent to launch our activity if the user selects this notification
+       PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+       new Intent(this, MainActivity.class), 0);
+
+        // Set the info for the views that show in the notification panel.
+
+        Notification notification = new Notification.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)   // the status icon
+            .setTicker("Monitoring emergencies...")// the status text
+            .setWhen(System.currentTimeMillis())    // the time stamp
+            .setContentTitle("Too Much Sauce") // the label of the entry
+            .setContentText("Currently monitoring any emergency.")   // the contents of the entry
+            .setContentIntent(contentIntent)    // The intent to send when the entry is clicked
+            .setOngoing(true)       //You can't clear this bitch. Yo understood?!@
+            .build();
+
+        // Send the notification.
+        mNM.notify(REMINDER_NOTIFICATION, notification);
+
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        //Cancel persistent notification.
+        mNM.cancel(REMINDER_NOTIFICATION);
+
+        //Cancel sensor shake listener
+        if(mSensorManager != null)
+            mSensorManager.unregisterListener(mShakeDetector);
+
+        Toast.makeText(this, "Service stopped.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void setSpeed(int speed)
+    {
+        Log.d("SPPPPPEEEEEEEEEDDDDD", "Speed set at " + speed);
+        Toast.makeText(this, "Speed set at " + speed, Toast.LENGTH_SHORT).show();
+        currentSpeed = speed;
+    }
+
+    public int getSpeed()
+    {
+        Toast.makeText(this, "Speed returned " + currentSpeed, Toast.LENGTH_SHORT).show();
+        return currentSpeed;
+    }
+
+
 }
--- app/src/main/java/edu/uf/mc/sauce/toomuchsos/MainActivity.java
+++ app/src/main/java/edu/uf/mc/sauce/toomuchsos/MainActivity.java
@@ -1,13 +1,202 @@
 package edu.uf.mc.sauce.toomuchsos;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.narayanacharya.waveview.WaveView;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener
+{
+    boolean isBound, isRunning;
+    int speed;
+
+    Intent serviceIntent;
+    CrashDetectionService crashDetectionService;
+
+    TextView titleLeft, titleRight, speedLabel;
+    SeekBar speedSlider;
+    Button serviceButton;
+    WaveView background;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Sketch.ttf");
+
+        titleLeft = (TextView) findViewById(R.id.titleLeft);
+        titleLeft.setTypeface(tf);
+        titleRight = (TextView) findViewById(R.id.titleRight);
+        titleRight.setTypeface(tf);
+        speedLabel = (TextView) findViewById(R.id.currentSpeedIndicator);
+        background = (WaveView) findViewById(R.id.waveView);
+        int bgColor = Color.rgb(189, 221, 255);
+        background.setBackgroundColor(bgColor);
+        background.setWaveColor(Color.WHITE);
+
+        speedSlider = (SeekBar) findViewById(R.id.speedBar);
+        speedSlider.setOnSeekBarChangeListener(this);
+        speedSlider.setMax(140);
+
+        serviceButton = (Button) findViewById(R.id.serviceToggleButton);
+        serviceButton.setOnClickListener(this);
+
+
+        //Real stuff below.
+
+        //TODO: Load speed from service.
+        //startService(new Intent(this, CrashDetectionService.class));
+        serviceIntent = new Intent(MainActivity.this, CrashDetectionService.class);
+        serviceIntent.putExtra("Speed", speed+"");
+
+        isRunning = isServiceRunning();
+        if(isRunning)
+        {
+            //Initialize the button correctly
+            serviceButton.setText("STOP SERVICE");
+
+            //Bind the service
+            bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+            //Speed will be set when bindService completes
+        }
+        else
+        {
+            //Button is already initialized correctly. nvm.
+            serviceButton.setText("START SERVICE");
+        }
+
     }
+
+    //Next 3 methods override Seekbar value change.
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
+    {
+        speed = progress;
+        speedLabel.setText(speed + "");
+        if(serviceIntent != null)
+            serviceIntent.putExtra("Speed", speed+"");
+
+        //TODO: Notify the service
+        if(crashDetectionService != null)
+        {
+            crashDetectionService.setSpeed(speed);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+
+    //Next method handles button press. Only one badass button here!
+    @Override
+    public void onClick(View view)
+    {
+        if(serviceButton.getText().equals("START SERVICE"))
+        {
+            //Start the service
+            if(serviceIntent != null)
+            {
+                //Start the service
+                getApplicationContext().startService(serviceIntent);
+
+                //Bind the service
+                bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+                isRunning = true;
+            }
+
+            serviceButton.setText("STOP SERVICE");
+        }
+        else
+        {
+            //Stop the service
+            //idk if this is gonna work
+            if(serviceIntent != null)
+            {
+                //TODO: Unbind.
+                getApplicationContext().stopService(serviceIntent);
+                isRunning = false;
+            }
+
+            serviceButton.setText("START SERVICE");
+        }
+    }
+
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        //UnBind from service
+        if(isBound)
+        {
+            unbindService(serviceConnection);
+            isBound = false;
+        }
+    }
+
+
+    private boolean isServiceRunning()
+    {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE))
+        {
+            if (CrashDetectionService.class.getName().equals(service.service.getClassName()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    private ServiceConnection serviceConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder)
+        {
+            Toast.makeText(MainActivity.this, "Activity bound?", Toast.LENGTH_SHORT).show();
+            CrashDetectionService.SOSBinder mBinder = (CrashDetectionService.SOSBinder) iBinder;
+            crashDetectionService = mBinder.getService();
+
+            //Set the speed when activity binds to service. (0 initially)
+            speed = crashDetectionService.getSpeed();
+            speedSlider.setProgress(speed);
+            speedLabel.setText(speed + "");
+
+            isBound = true;
+            isRunning = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName)
+        {
+            Toast.makeText(MainActivity.this, "Activity unbound?", Toast.LENGTH_SHORT).show();
+            crashDetectionService = null;
+            isBound = false;
+        }
+    };
+
 }
